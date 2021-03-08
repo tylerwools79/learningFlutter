0b2bb868db4602b3fcbda9c3d7c13fa8a838332c
tylerwools79 <tylerwools79@gmail.com>
2021-03-08 14:11:39 -0700

    Enabled hot-reload. played with buttons and images. several lines of code are commented out for the sake of brevity.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+  ));
+
+
+//this is part of the tutorial #7, for hot reload.
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('My first app'),
         centerTitle: true,
         backgroundColor: Colors.teal[300],
       ),
       body: Center(
-        child: Text(
+        child: IconButton(
+          onPressed: () {
+            print('You clicked @');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.pink
+        )/*child: RaisedButton(
+          onPressed: (){
+            print('you clicked me');
+          },
+          child: Text('Hello')
+        ),*/
+/*        child: Text(
           'Hello World!',
           style: TextStyle(
             fontSize: 20.0,
             fontWeight: FontWeight.bold,
             letterSpacing: 2.0,
             color: Colors.grey,
-            fontFamily: 'Akaya',
+            //fontFamily: 'Akaya',
           ),
-        ),
+        ),*/
+        //child: Image.asset('assets/default.jpg'),
+        //image.network('') //this is how to do stuff
+        /*child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.blue,
+          size: 50.0,
+        ),*/
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('Click me'),
+        child: Text('Clicck'),
         backgroundColor: Colors.teal[300],
       ),
-    ),
-  ));
+    );
+  }
+}
+
